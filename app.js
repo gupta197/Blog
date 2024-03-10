9affdf4bb01d560fa69873895255b6b7f8b4dcc7
Vikas Gupta <vg229297@gmail.com>
2024-03-10 20:44:13 +0530

    add database connection with mysql

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mysql = require("mysql");
+var session = require("express-session");
+var con = mysql.createConnection({
+  host : "localhost",
+  user:"root",
+  password :"",
+  database:"blogs"
+});
+con.connect((error)=>{
+  if(error) throw error;
+  console.log("connected!!!")
+});
+global.con = con;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret:"bloging",resave:false,saveUninitialized:true}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

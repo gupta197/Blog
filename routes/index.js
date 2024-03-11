22f25f01fb5c90c789e82113dddc1f3c3d34649b
Vikas Gupta <vg229297@gmail.com>
2024-03-11 22:22:25 +0530

    Add register ejs to render register html page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* GET home page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+router.post('/register', function(req, res) {
+  const {email, password, username} = req.body;
+  if(email && email.length && password && password.length && username && username.length){
+      
+  }else{
+    res.send("All fields are required !!!")
+  }
+  
+});
 module.exports = router;

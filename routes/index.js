ef380db195982a87a10f439d38a6fecab6612a57
gupta197 <vg2292097@gmail.com>
2024-03-12 20:48:08 +0530

    Check email is present on table or not in register api

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,26 @@ router.get('/', function(req, res, next) {
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
-router.post('/register', function(req, res) {
-  const {email, password, username} = req.body;
-  if(email && email.trim().length && password && password.trim().length && username && username.trim().length){
-    
-      
-  }else{
-    res.render('register', { title: 'Register', error: "All Fields are requied" });
+router.post('/register', async function(req, res) {
+  try {
+    const {email, password, username} = req.body;
+    if(email && email.trim().length && password && password.trim().length && username && username.trim().length){
+      let query = `SELECT user_id, username FROM users WHERE email LIKE '${email}'`
+      con.query(query,(error, rows, fields)=>{
+        if(!error && rows.length >= 1){
+          return res.render('register', { title: 'Register', error: "Email Already exists!!" });
+        }
+        
+      })
+        
+    }else{
+     return res.render('register', { title: 'Register', error: "All Fields are requied" });
+    }
+  } catch (error) {
+    console.log("Error in register", error.message)
+    return res.render('register', { title: 'Register', error: "Something went wrong!!" });
   }
+
   
 });
 module.exports = router;

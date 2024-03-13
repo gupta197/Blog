275b55ade3c495d7736f474abdec3644322b8913
gupta197 <vg2292097@gmail.com>
2024-03-13 20:31:38 +0530

    Add signup code and handle success and error message

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,39 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* GET home page. */
+/* GET signup page. */
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register', error:"" });
+  res.render('register', { title: 'Register', error:"", message:"" });
 });
+/* POST signup API. */
 router.post('/register', async function(req, res) {
   try {
     const {email, password, username} = req.body;
     if(email && email.trim().length && password && password.trim().length && username && username.trim().length){
-      let query = `SELECT user_id, username FROM users WHERE email LIKE '${email}'`;
+      let query = `SELECT user_id, username FROM users WHERE email = '${email}'`;
       con.query(query,(error, rows, fields)=>{
-        console.log(error, rows, fields);
         if(!error && rows.length >= 1){
-          return res.render('register', { title: 'Register', error: "Email Already exists!!" });
+          return res.render('register', { title: 'Register', error: "Email Already exists!!", message:"" });
         }
+        if(error){
+          return res.render('register', { title: 'Register', error: "Something went wrong!!", message:"" });
+        }
+        query = `INSERT INTO users (username, email,password) VALUES ('${username}','${email}','${password}');`;
+        con.query(query, (error, rows, fields)=>{
+          if(error){
+            return res.render('register', { title: 'Register', error: "Something went wrong!!" , message:"" });
+          }
+          return res.render('register', { title: username , error: "" ,message: `User register successfully` });
+        })
 
       })
         
     }else{
-     return res.render('register', { title: 'Register', error: "All Fields are requied" });
+     return res.render('register', { title: 'Register', error: "All Fields are requied", message:"" });
     }
   } catch (error) {
     console.log("Error in register", error.message)
-    return res.render('register', { title: 'Register', error: "Something went wrong!!" });
+    return res.render('register', { title: 'Register', error: "Something went wrong!!" , message:""});
   }
 
   

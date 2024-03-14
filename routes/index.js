fb24b8a0e8c2e6bf630c982c25dc7428778a049b
gupta197 <vg2292097@gmail.com>
2024-03-14 20:38:42 +0530

    Add Login and sign button to respective pages

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Blogs' });
 });
 /* GET signup page. */
 router.get('/register', function(req, res, next) {

f91efb9baffa0d607f0a4b7c721e3ccd0c2168af
gupta197 <vg2292097@gmail.com>
2024-04-06 18:33:08 +0530

    index routes optimise

--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,31 @@ router = express.Router();
 /* GET home page. */
 router.get('/', async function(req, res) {
   try {
-    let message = req.session.email ? "User register successfully. Please login!!" : "";
-    if(message.length){
-      return res.redirect('user');
-    }else{
       let records = await postController.getAllPost(req,res);
       return sendResponse(req, res, {page : 'index', records: records});
-    }
-    
   } catch (error) {
     return sendResponse(req, res, {page : 'index', records: []});
   }
 });
+
 /* GET signup page. */
 router.get('/register', authController.getRegisterPage);
+
 /* POST signup API. */
 router.post('/register', authController.postRegisterPage );
+
 /* GET login page. */
 router.get('/login', authController.getLoginPage);
+
+/* Post login page. to login the user */
 router.post('/login', authController.postLoginPage);
+
+/* Logout API. In this api we clear the session */
 router.get('/logout', authController.logout);
-router.get('/about', async (req,res)=>{
-  let isUserLoggedIn = req.session.email ? true: false;
-  return res.render('aboutus', { title: 'Blogs', isUserLoggedIn: isUserLoggedIn , name: req.session.username || ""});
+
+/* about us API. In this api render the static page*/
+router.get('/about', async (req,res)=>{  
+  return sendResponse(req, res, {page : 'aboutus', pageTitle : "Blogs"});
 });
 
 module.exports = router;

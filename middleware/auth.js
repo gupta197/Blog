4c8511bbeb440ad018eaaf6c8b0d385a9a73ac78
gupta197 <vg2292097@gmail.com>
2024-04-06 21:10:08 +0530

    Handle user name issue on login page and fix some placeholder issue

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 const {sendResponse} = require('../commonFunction/commonFunction');
 module.exports.isUserLoggedIn = async (req ,res ,next ) => {
-  console.log("here",req.session.email)
     if(req.session.email){
       next();
       return;
